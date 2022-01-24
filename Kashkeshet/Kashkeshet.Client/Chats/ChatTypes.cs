74e66e23bba3c59a6c0226a931445c47190a17e1
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 10:49:38 +0200

    Added enum ChatTypes

---
+++ Kashkeshet/Kashkeshet.Client/Chats/ChatTypes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kashkeshet.Client.Chats
+{
+    public enum ChatTypes
+    {
+        Global, Group, Private 
+    }
+}
