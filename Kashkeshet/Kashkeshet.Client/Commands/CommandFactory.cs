248e5502980dd0737f50a95834d4f28ac88a3156
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 11:35:22 +0200

    Added CommandFactory class

---
+++ Kashkeshet/Kashkeshet.Client/Commands/CommandFactory.cs
@@ -0,0 +1,13 @@
+﻿using Kashkeshet.Client.Chats;
+using System.Collections.Generic;
+
+namespace Kashkeshet.Client.Commands
+{
+    public class CommandFactory
+    {
+        public ICommand Create(string command, byte[] msg, List<ChatInfo> chats)
+        {
+            return null;
+        }
+    }
+}
