0a6eee4e8e584aef95d9e9521c17b7c7c031eee3
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 20:57:43 +0200

    Added abstract class OptionBase

---
+++ Kashkeshet/Kashkeshet.Client.UI/OptionBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.UI
+{
+    public abstract class OptionBase
+    {
+        public string Description { get; private set; }
+
+        public abstract void Run();
+    }
+}
