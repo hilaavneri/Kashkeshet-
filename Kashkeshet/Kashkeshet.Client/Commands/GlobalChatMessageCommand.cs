164574b430566e40b803c85e725d19f1539f7d33
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 12:03:25 +0200

    Added GlobalChatMessageCommand class

--- Kashkeshet/Kashkeshet.Client/Commands/GlobalChatMessageCommand.cs
+++ Kashkeshet/Kashkeshet.Client/Commands/GlobalChatMessageCommand.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Kashkeshet.Client.Chats;
+using Kashkeshet.Client.Messages;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Kashkeshet.Client.Commands
 {
-    class GlobalChatMessageCommand
+    class GlobalChatMessageCommand : ICommand
     {
+        private List<Chat> _chats;
+        private ChatMessageInfo _message;
+
+        public void Run()
+        {
+            var globalChats = _chats.Where(chat => chat.type == ChatTypes.Global);
+            globalChats.ToList().ForEach(chat => chat.AddMessage(_message));
+        }
     }
 }
