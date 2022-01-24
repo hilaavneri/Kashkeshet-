6703151426b265870c87df56b47ed5c473093a89
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 20:08:33 +0200

    implemented Run method at GetUsernamesCommand

--- Kashkeshet/Kashkeshet.Server/Commands/GetUsernamesCommand.cs
+++ Kashkeshet/Kashkeshet.Server/Commands/GetUsernamesCommand.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Kashkeshet.Server.Commands
 {
-    class GetUserCommand
+    class GetUsernamesCommand : ICommand
     {
+        private string _name;
+        private ClientsMsgs _messages;
+        private int _id;
+
+        public void Run()
+        {
+            string usernames = "";
+            _messages.GetAllUserNames().ForEach(username => usernames += username + "|");
+            var toSend = Encoding.ASCII.GetBytes(_name).Concat(Encoding.ASCII.GetBytes(usernames)).ToArray();
+            _messages.SendMessageById(_id, toSend);
+        }
     }
 }
