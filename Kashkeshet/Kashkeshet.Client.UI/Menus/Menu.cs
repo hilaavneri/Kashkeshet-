f6eefc865dc61a5a1f122e4d25c7de824393bf45
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 21:01:34 +0200

    Added class Menu

---
+++ Kashkeshet/Kashkeshet.Client.UI/Menus/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.UI.Menus
+{
+    class Menu<T>
+    {
+        private Dictionary<T, OptionBase> _options;
+
+        public Menu(Dictionary<T, OptionBase> options)
+        {
+            _options = options;
+        }
+
+        public void RunOption(T key)
+        {
+            _options[key]?.Run();
+        }
+
+        public void AddOption(T key, OptionBase option)
+        {
+            _options[key] = option;
+        }
+    }
+}
--- Kashkeshet/Kashkeshet.Client.UI/Menus/OptionBase.cs
+++ Kashkeshet/Kashkeshet.Client.UI/Menus/OptionBase.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Client.UI
+namespace Client.UI.Menus
 {
     public abstract class OptionBase
     {
