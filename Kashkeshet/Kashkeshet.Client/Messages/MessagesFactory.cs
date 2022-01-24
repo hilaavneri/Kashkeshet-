4990b2e05620d86c8a71fce26be5b54c3c87ba5a
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 13:28:38 +0200

    Implemented methods at MessagesFactory class

--- Kashkeshet/Kashkeshet.Client/Messages/MessagesFactory.cs
+++ Kashkeshet/Kashkeshet.Client/Messages/MessagesFactory.cs
@@ -1,14 +1,24 @@
-﻿using System;
+﻿using Kashkeshet.Client.Chats;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Kashkeshet.Client.Messages
 {
     public class MessagesFactory
     {
+        private int _userNameLength;
+
+        public MessagesFactory(int userNameLength)
+        {
+            _userNameLength = userNameLength;
+        }
+
         public ChatMessageInfo Create(byte[] data)
         {
-            return null;
+            string username = Encoding.ASCII.GetString(data.Take(_userNameLength).ToArray());
+            return new ChatMessageInfo(ChatTypes.Global, username, data.Skip(_userNameLength).ToArray());
         }
     }
 }
