54698a5066b821084642f0e67ca10ac8615dc126
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 11:06:53 +0200

    Added class GroupChatMessageInfo

---
+++ Kashkeshet/Kashkeshet.Client/Messages/GroupChatMessageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kashkeshet.Client.Chats;
+
+namespace Kashkeshet.Client.Messages
+{
+    public class GroupChatMessageInfo : ChatMessageInfo
+    {
+        public string GroupName { get; private set; }
+
+        public GroupChatMessageInfo(ChatTypes chatType, string senderUserName, byte[] message, string groupName) : base(chatType, senderUserName, message)
+        {
+            GroupName = groupName;
+        }
+    
+    }
+}
