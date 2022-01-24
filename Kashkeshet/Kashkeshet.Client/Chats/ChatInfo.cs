e12e554df37032e7d91a15a13e82a7d77730d426
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 11:13:39 +0200

    Added Properties and construcor to ChatInfo class

--- Kashkeshet/Kashkeshet.Client/Chats/ChatInfo.cs
+++ Kashkeshet/Kashkeshet.Client/Chats/ChatInfo.cs
@@ -1,7 +1,19 @@
-﻿namespace Kashkeshet.Client.Chats
+﻿using Kashkeshet.Client.Messages;
+using System.Collections.Generic;
+
+namespace Kashkeshet.Client.Chats
 {
     public class ChatInfo
     {
-        
+
+        public List<ChatMessageInfo> Messages { get; private set; }
+        public ChatTypes type { get; private set; }
+
+        public ChatInfo(List<ChatMessageInfo> messages, ChatTypes type)
+        {
+            Messages = messages;
+            this.type = type;
+        }
+
     }
 }
