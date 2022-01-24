013d41bb83e51afac54f582c79d2247787697a1e
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 14:10:23 +0200

    Added ConsoleDataWriter class

---
+++ Kashkeshet/Kashkeshet.Client.UI/ConsoleDataWriter.cs
@@ -0,0 +1,15 @@
+﻿using Kashkeshet.Client.Common;
+using System;
+using System.Text;
+
+namespace Kashkeshet.Client.UI
+{
+    public class ConsoleDataWriter : IWriter
+    {
+        public byte[] GetData()
+        {
+            string data = Console.ReadLine();
+            return Encoding.ASCII.GetBytes(data);
+        }
+    }
+}
