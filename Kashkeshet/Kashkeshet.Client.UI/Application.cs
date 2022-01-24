7a23ef4bf522517970980e101638dbc932298834
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 14:51:39 +0200

    Added bootstrapper

---
+++ Kashkeshet/Kashkeshet.Client.UI/Application.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kashkeshet.Client.UI
+{
+    class Application
+    {
+    }
+}
---
+++ Kashkeshet/Kashkeshet.Client/Bootsrapper.cs
@@ -0,0 +1,26 @@
+﻿using Kashkeshet.Client.Chats;
+using Kashkeshet.Client.Clients;
+using Kashkeshet.Client.Commands;
+using Kashkeshet.Client.Messages;
+using Kashkeshet.Common.SendRecv;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Kashkeshet.Client
+{
+    public class Bootsrapper
+    {
+        public ChatClient Create()
+        {
+            TcpClient client = new TcpClient();
+            client.GetStream().ReadTimeout = 1000;
+            var chat = new Chat(new List<ChatMessageInfo>(), ChatTypes.Global);
+            var chats = new List<Chat>();
+            chats.Add(chat);
+            return new ChatClient(new TcpSendRecv(client.GetStream()), new CommandFactory(), new MessagesFactory(20), chats);
+
+        }
+    }
+}
