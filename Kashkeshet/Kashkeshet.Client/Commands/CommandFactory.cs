2a82bb38f335c58cf4e128b3530ef6b2c7711ffa
Hila Avneri <hila.avneri@gmail.com>
2022-01-24 13:20:35 +0200

    Added send all case to thre Create method at CommandFactory class

--- Kashkeshet/Kashkeshet.Client/Commands/CommandFactory.cs
+++ Kashkeshet/Kashkeshet.Client/Commands/CommandFactory.cs
@@ -1,12 +1,17 @@
 ﻿using Kashkeshet.Client.Chats;
+using Kashkeshet.Client.Messages;
 using System.Collections.Generic;
 
 namespace Kashkeshet.Client.Commands
 {
     public class CommandFactory
     {
-        public ICommand Create(string command, byte[] msg, List<Chat> chats)
+        public ICommand Create(string command, byte[] msg, List<Chat> chats, MessagesFactory messagesFactory)
         {
+            if (command.Equals("SNDALL"))
+            {
+                return new GlobalChatMessageCommand(chats, messagesFactory.Create(msg));
+            }
             return null;
         }
     }
